2f172d962825b3bb199e0e7d0664424639966a1f
Paul Fuller <pfuller@360view.com>
2020-10-12 19:09:14 -0700

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tsf-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'type-safe-forms';
+}
